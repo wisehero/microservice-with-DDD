631477c372c9215a4515f73b560397a1a885e504
wisehero10 <godjw96@gmail.com>
2022-12-26 02:02:07 +0900

    Initial version of book generated by generator-jhipster@7.9.3

---
+++ book/src/test/java/com/my/book/IntegrationTest.java
@@ -0,0 +1,22 @@
+package com.my.book;
+
+import com.my.book.BookApp;
+import com.my.book.config.AsyncSyncConfiguration;
+import com.my.book.config.EmbeddedSQL;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+
+/**
+ * Base composite annotation for integration tests.
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@SpringBootTest(classes = { BookApp.class, AsyncSyncConfiguration.class })
+@EmbeddedSQL
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_CLASS)
+public @interface IntegrationTest {
+}
