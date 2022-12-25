ab6f91b2a3ceabbf850dddd9a89511644db542a1
wisehero10 <godjw96@gmail.com>
2022-12-26 00:59:45 +0900

    Initial version of gateway generated by generator-jhipster@7.9.3

---
+++ gateway/src/main/webapp/app/router/index.ts
@@ -0,0 +1,58 @@
+import Vue from 'vue';
+import Component from 'vue-class-component';
+Component.registerHooks([
+  'beforeRouteEnter',
+  'beforeRouteLeave',
+  'beforeRouteUpdate', // for vue-router 2.2+
+]);
+import Router, { RouteConfig } from 'vue-router';
+
+const Home = () => import('@/core/home/home.vue');
+const Error = () => import('@/core/error/error.vue');
+import account from '@/router/account';
+import admin from '@/router/admin';
+import entities from '@/router/entities';
+import pages from '@/router/pages';
+
+Vue.use(Router);
+
+// prettier-ignore
+const router = new Router({
+  mode: 'history',
+  routes: [
+    {
+      path: '/',
+      name: 'Home',
+      component: Home
+    },
+    {
+      path: '/forbidden',
+      name: 'Forbidden',
+      component: Error,
+      meta: { error403: true }
+    },
+    {
+      path: '/not-found',
+      name: 'NotFound',
+      component: Error,
+      meta: { error404: true }
+    },
+    ...account,
+    ...admin,
+    entities,
+    ...pages
+  ]
+});
+
+export const lazyRoutes = Promise.all([
+  import('@libararyview/entities-router')
+    .then(libararyviewRouter => {
+      router.addRoute(libararyviewRouter.default as RouteConfig);
+      return libararyviewRouter.default;
+    })
+    .catch(() => {
+      console.log("Error loading libararyview menus. Make sure it's up.");
+    }),
+]);
+
+export default router;
